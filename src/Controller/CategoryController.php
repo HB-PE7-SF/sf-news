0e0991b9d205ca28bcd1a77629c235a241640b64
Lucas Delobelle <lucas@ld-web.net>
2023-11-20 14:00:42 +0100

    :sparkles: add Category Form

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
@@ -25,7 +29,7 @@ class CategoryController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'category_item')]
+    #[Route('/{id<\d+>}', name: 'category_item')]
     public function item(int $id): Response
     {
         $category = $this->categoryRepository->find($id);
@@ -37,5 +41,32 @@ class CategoryController extends AbstractController
         return $this->render('category/item.html.twig', [
             'category' => $category
         ]);
+    }
+
+    #[Route('/new', name: 'category_new')]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        // Je crée une instance de l'entité
+        // et je la lie au formulaire
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+
+        // Avec la requête entrante, je vais vouloir traiter les données du formulaire
+        $form->handleRequest($request);
+
+        // Une fois la requête entrante traitée (si des données s'y trouvent)
+        // Je vais vérifier si le formulaire a bien été soumis et est valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            // persister l'entité $category
+            $em->persist($category);
+            $em->flush();
+
+            return $this->redirectToRoute('categories_list');
+        }
+
+        return $this->renderForm(
+            'category/new.html.twig',
+            ['category_form' => $form]
+        );
     }
 }
---
+++ src/Form/CategoryType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('Enregistrer', SubmitType::class, [
+                'attr' => [
+                    'class' => 'bg-blue-600 hover:bg-blue-800 text-white px-5 py-2'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+}
