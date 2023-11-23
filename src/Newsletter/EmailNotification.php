01f134769451e9ba0816659520be2075707f4c81
Lucas Delobelle <lucas@ld-web.net>
2023-11-23 11:20:21 +0100

    :sparkles: add Application parameter
    admin email

--- src/Newsletter/EmailNotification.php
+++ src/Newsletter/EmailNotification.php
@@ -9,14 +9,15 @@ use Symfony\Component\Mime\Email;
 class EmailNotification
 {
     public function __construct(
-        private MailerInterface $mailer
+        private MailerInterface $mailer,
+        private string $adminEmail
     ) {
     }
 
     public function confirmSubscription(NewsletterEmail $newsletterEmail): void
     {
         $email = (new Email())
-          ->from('admin@mycorp.info')
+          ->from($this->adminEmail)
           ->to($newsletterEmail->getEmail())
           ->subject("Merci pour votre inscription")
           ->text("Votre email " . $newsletterEmail->getEmail() . " a bien été enregistré dans notre newsletter");
